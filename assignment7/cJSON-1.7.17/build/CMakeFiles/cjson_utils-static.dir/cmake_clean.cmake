cc2247c8a1f1220a1e8672fa5fef8b5ea7752448
briantran2019 <briantran2019@gmail.com>
2024-05-07 23:44:16 +0000

    first

---
+++ assignment7/cJSON-1.7.17/build/CMakeFiles/cjson_utils-static.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cjson_utils-static.dir/cJSON_Utils.c.o"
+  "CMakeFiles/cjson_utils-static.dir/cJSON_Utils.c.o.d"
+  "libcjson_utils.a"
+  "libcjson_utils.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cjson_utils-static.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ assignment7/cJSON-1.7.17/build/CMakeFiles/cjson_utils.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cjson_utils.dir/cJSON_Utils.c.o"
+  "CMakeFiles/cjson_utils.dir/cJSON_Utils.c.o.d"
+  "libcjson_utils.pdb"
+  "libcjson_utils.so"
+  "libcjson_utils.so.1"
+  "libcjson_utils.so.1.7.17"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cjson_utils.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
